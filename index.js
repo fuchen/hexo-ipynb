cd758c00cb35f6f17d5af65e6f49c91f67ab4ad6
fuchen <ink.pad@163.com>
2017-04-28 15:42:16 +0800

    Remove the processor to avoid page auto-update when .ipynb file changed.

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 const path = require('path')
 const exec = require('co-exec')
 const co = require('co')
-const fs = require('co-fs')
-
-function findPostFromAsset(file) {
-  const Post = hexo.model('Post')
-  const posts = Post.toArray()
-
-  for (var i = 0, len = posts.length; i < len; i++) {
-    let post = posts[i]
-    if (file.source.startsWith(post.asset_dir)) {
-      return post
-    }
-  }
-}
-
-function processPostAsset(file) {
-  return co(function *() {
-    if (file.type !== 'skip') {
-      let post = findPostFromAsset(file)
-      if (post) {
-        post.updated = new Date(0)
-        post.save()
-      }
-    }
-  })
-}
-
-hexo.extend.processor.register(/\.ipynb$/, function (file) {
-  if (file.path.startsWith('_posts/') || file.path.startsWith('_drafts/')) {
-    return processPostAsset(file)
-  }
-})
 
 const PY_SCRIPT = path.join(__dirname, 'convert.py')
 
